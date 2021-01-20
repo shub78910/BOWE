8c832e24adf65baa428a5600ec6a421e1f1dae47
shub78910 <64699670+shub78910@users.noreply.github.com>
2021-01-20 14:57:30 +0530

    update

--- app.js
+++ app.js
@@ -22,3 +22,35 @@ open.addEventListener("click", function(){
         changeIcon = true;
     }
 })
+
+var divs = [
+    " We provide you digital solutions for your business, by creating user friendly websites and also, adding features on your existing websites.",
+    "Content Writing text here",
+    "App dev text here."
+] 
+
+var images = ["images used/webdev.JPG",
+            "images used/comtentwriting.JPG",
+            "images used/anddev.JPG"]
+
+var headings=["Web Development","Content Writing","App development"]
+
+let i=0;
+
+function slides(){
+    document.querySelector(".image").src = images[i]
+    document.querySelector(".desc").innerText = divs[i];
+    document.querySelector(".heading").innerText = headings[i];
+    
+    // console.log(images[i])
+    // console.log(divs[i])
+
+
+    if (i<divs.length-1){
+        i++
+    }else{
+        i=0;
+    }
+}
+
+setInterval (slides, 2000);
